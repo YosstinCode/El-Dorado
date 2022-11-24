e8b76ef57c3ef05fbc8ac6745ffa18384aed43ab
YosstinCode <yosstincode@gmail.com>
2022-11-23 19:06:47 -0500

    config for railway deployment

--- inventario/settings.py
+++ inventario/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-0wm0@gej-kd)oeygo58bqnh0(0v%vlm7dj5!(vfw3#!^7=abh4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
